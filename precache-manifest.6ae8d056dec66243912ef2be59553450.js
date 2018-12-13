d8ae05d28829bf7644d848714079a0430d8444f6
jonathanmng <31453488+jonathanMNg@users.noreply.github.com>
2018-12-13 13:51:30 -1000

    Updates

---
+++ precache-manifest.6ae8d056dec66243912ef2be59553450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "9f839d09ce85f2d9931f",
+    "url": "/ravenous/static/js/runtime~main.9f839d09.js"
+  },
+  {
+    "revision": "7c7468453760f33eaf96",
+    "url": "/ravenous/static/js/main.7c746845.chunk.js"
+  },
+  {
+    "revision": "d94fc685c375ad0a6b02",
+    "url": "/ravenous/static/js/1.d94fc685.chunk.js"
+  },
+  {
+    "revision": "7c7468453760f33eaf96",
+    "url": "/ravenous/static/css/main.844053dc.chunk.css"
+  },
+  {
+    "revision": "3193d35931b77fa773ffdac8edcc8a27",
+    "url": "/ravenous/index.html"
+  }
+];
